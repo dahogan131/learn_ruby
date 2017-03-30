fb23f8a6659aa445accfecfb5ec8845d5dd2f657
Dae Hogan <email": null>
2017-03-29 19:52:55 -0700

    got rspec tests to pass

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,34 @@
 #write your code here
+def add(int1, int2)
+	p int1 + int2
+end
+
+def subtract(*int)
+	int.reduce(:-)
+end
+
+def sum(int)
+	return 0 if int.length == 0
+	int.reduce(:+)
+end
+
+def multiply(*int)
+	int.reduce(:*)
+end
+
+def power(base, exp)
+	base **exp
+end
+
+#4! = 4*3*2*1 = 24
+#loop through integers, multiplying each time
+def factorial(int)
+	return 0 if int == 0
+	index = 1
+	factorial = 1
+	until index == int
+		index +=1
+		factorial *= index
+	end
+	factorial	
+end
