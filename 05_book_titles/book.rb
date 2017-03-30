d4147f2cc48b7906edbb7e2c24b72e789f2e7af9
Dae Hogan <email": null>
2017-03-30 14:09:46 -0700

    Got program to pass specs.

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,22 @@
 class Book
 # write your code here
+attr_accessor :title 
+	def initialize
+		@title = "inferno"
+	end
+	
+	def title
+		# p @title.split.map(&:capitalize).join(" ")
+		
+		title_array = []
+		@title.split.map do |word| 
+			if word == "and" || word == "of" || word == "in" || word == "the" || word == "a" || word == "an"
+				title_array << word
+			else
+				title_array << word.capitalize
+			end
+		end
+		title_array.first.capitalize!
+		title_array.join(" ")
+	end
 end
